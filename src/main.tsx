202009a4295d13fcbed6f15b8cf8949a2a7af624
AlexKorzh <korzhlesha@gmail.com>
2023-09-23 12:50:07 +0200

    added hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import {store} from "./store";
+import { store } from "./store";
 import { Provider } from 'react-redux';
 
+// This code checks for the hash on load
+if (window.location.hash) {
+  const [path, search] = window.location.hash.slice(1).split('?');
+  history.replaceState({}, '', `${path}${search}`);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import Copy from 'vite-plugin-copy'
 
 export default defineConfig({
   base: '/listings/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    Copy({
+      targets: [
+        { src: '404.html', dest: 'dist' }
+      ]
+    })
+  ],
 })
